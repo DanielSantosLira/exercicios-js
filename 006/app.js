0f0615fe0e2947058f17df10b1e99c1c71de1d2b
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2022-11-13 14:01:24 -0300

    Create app.js

---
+++ 006/app.js
@@ -0,0 +1,5 @@
+document.getElementById('titulo').
+    innerHTML = 'você ganha por hora?'
+    
+    document.getElementById('subtitulo').
+    innerHTML = 'quanto você ganha de hora extra por mês?'
