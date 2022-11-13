6be9b2b65d4330a8ce8355a398cbef22c9915e45
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2022-11-13 13:37:46 -0300

    Update app.js

--- 003/app.js
+++ 003/app.js
@@ -3,7 +3,7 @@ function Somar(){
     let valor1 = document.getElementById('n1').value
     let valor2 = document.getElementById('n2').value
      
-    let total = parseFloat(valor1) + parseFloat(valor2)
+    let total = (parseFloat(valor1) + parseFloat(valor2))
 
     document.getElementById('resposta').innerHTML = total
 }
